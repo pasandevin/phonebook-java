986048e39a2a237bee5858367ad6c25114036fc0
pasandevin <jayawardene.pasandevin@gmail.com>
2023-07-12 22:55:21 +0530

    Add constants for Strings in user interactions

--- src/PhoneBook.java
+++ src/PhoneBook.java
@@ -10,15 +10,15 @@ public class PhoneBook {
 
     public void addContact(Contact contact) {
         contacts.put(contact.getName(), contact);
-        System.out.println("Contact added: " + contact.getName());
+        System.out.println(UIConstants.CONTACT_ADDED_MSG+ contact.getName());
     }
 
     public void searchContact(String name) {
         Contact contact = contacts.get(name);
         if (contact != null) {
-            System.out.println("Contact found: " + contact.getName() + " - " + contact.getPhoneNumber());
+            System.out.println(UIConstants.CONTACT_FOUND_MSG + contact.getName() + " - " + contact.getPhoneNumber());
         } else {
-            System.out.println("Contact not found: " + name);
+            System.out.println(UIConstants.CONTACT_NOT_FOUND_MSG + name);
         }
     }
 
@@ -26,23 +26,23 @@ public class PhoneBook {
         Contact contact = contacts.get(name);
         if (contact != null) {
             contact.setPhoneNumber(phoneNumber);
-            System.out.println("Contact updated: " + contact.getName());
+            System.out.println(UIConstants.CONTACT_UPDATED_MSG + contact.getName());
         } else {
-            System.out.println("Contact not found: " + name);
+            System.out.println(UIConstants.CONTACT_NOT_FOUND_MSG + name);
         }
     }
 
     public void deleteContact(String name) {
         Contact contact = contacts.remove(name);
         if (contact != null) {
-            System.out.println("Contact deleted: " + contact.getName());
+            System.out.println(UIConstants.CONTACT_DELETED_MSG + contact.getName());
         } else {
-            System.out.println("Contact not found: " + name);
+            System.out.println(UIConstants.CONTACT_NOT_FOUND_MSG + name);
         }
     }
 
     public void displayContacts() {
-        System.out.println("Contacts:");
+        System.out.println(UIConstants.CONTACTS_DISPLAY_MSG);
         for (Contact contact : contacts.values()) {
             System.out.println(contact.getName() + " - " + contact.getPhoneNumber());
         }
--- src/PhoneBookApplication.java
+++ src/PhoneBookApplication.java
@@ -13,27 +13,27 @@ public class PhoneBookApplication {
 
             switch (choice) {
                 case 1:
-                    System.out.print("Enter contact name: ");
+                    System.out.print(UIConstants.ASK_CONTACT_NAME_MSG);
                     String name = scanner.nextLine();
-                    System.out.print("Enter contact phone number: ");
+                    System.out.print(UIConstants.ASK_CONTACT_PHONE_NUMBER_MSG);
                     String phoneNumber = scanner.nextLine();
                     Contact contact = new Contact(name, phoneNumber);
                     phoneBook.addContact(contact);
                     break;
                 case 2:
-                    System.out.print("Enter contact name to search: ");
+                    System.out.print(UIConstants.ASK_CONTACT_NAME_TO_SEARCH_MSG);
                     String searchName = scanner.nextLine();
                     phoneBook.searchContact(searchName);
                     break;
                 case 3:
-                    System.out.print("Enter contact name to update: ");
+                    System.out.print(UIConstants.ASK_CONTACT_NAME_TO_UPDATE_MSG);
                     String updateName = scanner.nextLine();
-                    System.out.print("Enter new phone number: ");
+                    System.out.print(UIConstants.ASK_CONTACT_PHONE_NUMBER_TO_UPDATE_MSG);
                     String newPhoneNumber = scanner.nextLine();
                     phoneBook.updateContact(updateName, newPhoneNumber);
                     break;
                 case 4:
-                    System.out.print("Enter contact name to delete: ");
+                    System.out.print(UIConstants.ASK_CONTACT_NAME_TO_DELETE_MSG);
                     String deleteName = scanner.nextLine();
                     phoneBook.deleteContact(deleteName);
                     break;
@@ -41,10 +41,10 @@ public class PhoneBookApplication {
                     phoneBook.displayContacts();
                     break;
                 case 0:
-                    System.out.println("Exiting...");
+                    System.out.println(UIConstants.EXISTING_APP_MSG);
                     break;
                 default:
-                    System.out.println("Invalid choice!");
+                    System.out.println(UIConstants.INVALID_CHOICE_MSG);
                     break;
             }
         } while (choice != 0);
